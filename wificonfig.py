c391c6d286356d8f24a2afd8536dcf0fb9c92561
MIKE621 <miguelfuentes621@gmail.com>
2024-05-25 16:48:14 -0600

    configuration for connect and scan network wifi in raspberry pico W

---
+++ wificonfig.py
@@ -0,0 +1,38 @@
+import network
+from time import sleep
+
+
+class WifiConnect():
+    def connect():
+        try:
+            wifi = network.WLAN(network.STA_IF)
+            wifi.active(True)
+            wifi.connect('CLARO_9Wj2hd', 'fuentesgomez2022')
+            while wifi.isconnected() == False:
+                print('Wifi esta esperando conexión...')
+                sleep(1)
+            
+            ip = wifi.ifconfig()[0]
+            print(f'Wifi conexión establecida! IP:{ip}')
+            return True
+        except:
+            print('Algo anda mal!')
+            return False
+            
+            
+    def wifiscan():
+        try:
+            wifi = network.WLAN(network.STA_IF)
+            wifi.active(True)
+            nets = wifi.scan()
+            print('Lista de señales detectadas:')
+            for router in nets:
+                print('SSID:'+ str(router[0]))
+        except:
+            print('Algo anda mal!')
+        
+            
+#try:
+#    WifiConnect.connect('CLARO_9Wj2hd', 'fuentesgomez2022')
+#except keyboardinterrupt:
+#    machine.reset()
